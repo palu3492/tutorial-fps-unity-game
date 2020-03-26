0367f219e9332e020cbf281d1ea1038d88970526
Alex Palumbo <palu3492@stthomas.edu>
2020-03-26 01:46:21 -0500

    Setup level and score

---
+++ Block Game/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movementScript;
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.CompareTag("Obstacle"))
+        {
+            movementScript.enabled = false;
+        }
+    }
+}
---
+++ Block Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // rb.AddForce(0, 200, 500);
+    }
+
+    // Update is called once per frame
+    // Unity prefers FixedUpdate to Update when working with physics?
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce  * Time.deltaTime); // Move cube forward
+        // Checking for input should be done in Update()
+        // Move cube side to side
+        if ( Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
